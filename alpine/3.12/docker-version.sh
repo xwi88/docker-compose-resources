38b7bdc7ae1540304e89d1abe7ebaa300f91c1ee
xwi88 <278810732@qq.com>
2020-06-04 15:02:20 +0800

    rename dockerhub repo

--- alpine/3.12/docker-version.sh
+++ alpine/3.12/docker-version.sh
@@ -2,7 +2,7 @@
 
 # image info, version may auto update
 tag_version=3.12
-domain=xwi88
+domain=xwi999
 image_repo=${domain}/alpine
 # shellcheck disable=SC2034
 tag_name=${image_repo}:${tag_version}
--- golang/go1.14.4/docker-version.sh
+++ golang/go1.14.4/docker-version.sh
@@ -2,7 +2,7 @@
 
 # image info, version may auto update
 tag_version=1.14.4
-domain=xwi88
+domain=xwi999
 image_repo=${domain}/golang
 # shellcheck disable=SC2034
 tag_name=${image_repo}:${tag_version}
